2c0e6f9a621587450cb36149ec16ceecb644c0f6
Jaat0050 <rana.arjun52000@gmail.com>
2024-04-10 04:28:44 +0530

    room and player functionality added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 var server = http.createServer(app);
 
+const Room = require('./models/room');
+
 var io = require('socket.io')(server);
 
 //middle ware ==> { client -> middleware -> server }
@@ -17,8 +19,73 @@ const DB = "mongodb+srv://jaat0050:tictactoe123@tictactoe.cfxgfml.mongodb.net/?r
 
 io.on("connection", (socket) => {
     console.log("io connected!");
-    socket.on("createRoom", ({ nickname }) => {
+    socket.on("createRoom", async ({ nickname }) => {
         console.log(nickname);
+        console.log(socket.id);
+        try {
+            // room is created
+            let room = new Room();
+            let player = {
+                socketID: socket.id,
+                nickname,
+                playerType: 'X',
+            };
+            room.players.push(player);
+            room.turn = player;
+            // player is stored in the mongodb
+            room = await room.save();
+            console.log(room);
+            const roomId = room._id.toString();
+
+            socket.join(roomId);
+            // tell client room created 
+            // go to next page
+            io.to(roomId).emit("createRoomSuccess", room);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
+    socket.on("joinRoom", async ({ nickname, roomId }) => {
+        console.log(nickname);
+        console.log(roomId);
+        console.log(socket.id);
+        try {
+            // check room id
+            // if (roomId.match(/^[0-9a-fA-F]{24}$/)) {
+            if (!/^[0-9a-fA-F]{24}$/.test(roomId)) {
+                socket.emit('errorOccurred', 'Please enter valid room ID');
+                return;
+            }
+
+            let room = await Room.findById(roomId)
+
+            if (room.isJoin) {
+                let player = {
+                    nickname,
+                    socketID: socket.id,
+                    playerType: 'O',
+                }
+                socket.join(roomId);
+                room.players.push(player);
+                room.isJoin = false;
+
+                room = await room.save();
+
+                // tell client room created 
+                // go to next page
+                io.to(roomId).emit("joinRoomSuccess", room);
+                io.to(roomId).emit("updatePlayers", room.players);
+
+                io.to(roomId).emit("updateRoom", room);
+
+            } else {
+                socket.emit('errorOccurred', 'The game is in progress, try again later!');
+            }
+
+        } catch (e) {
+            console.log(e);
+        }
     });
 });
 
